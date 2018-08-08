f79f049ffcd74ed732e4f9c04f5a3f762dc3caf7
Casey Townsend <hello@cdalto.com>
2018-08-08 18:31:19 -0500

    Set value minutes

--- ClientApp/src/app/add-task/add-task.component.ts
+++ ClientApp/src/app/add-task/add-task.component.ts
@@ -7,9 +7,16 @@ import { NavMenuService } from '../services/nav-menu.service';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  estimatedTime: number;
+
   constructor(public nav: NavMenuService) { }
 
   ngOnInit(): void {
     this.nav.hide();
+  }
+
+  setRam(value) {
+    this.estimatedTime = value;
+    console.log(this.estimatedTime);
   }
 }
